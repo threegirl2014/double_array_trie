8da826aae6e3cb829740bd43297f123077c543d7
zhangrujia <zhangrujia@zhangrujias-MacBook-Pro.local>
2017-11-22 18:40:10 +0800

    search/delete/insert method complete

--- double_array_trie.py
+++ double_array_trie.py
@@ -48,78 +48,149 @@ class DoubleArrayTrie(object):
         self.next_pos = 1  # tails中下一个可插入的位置
         self.root = 1   # root节点
 
-    def search(self, word):
-        word += '#'
-        s = self.root
-        for i, c in enumerate(word):
-            n = self.chars[c]
-            if not n:  # 没有这个字符
-                return False
+    def check_word(self, word):
+        return False if '#' in word else True
 
-            if self.base[s] < 0:
-                pos = -self.base[s]
-                return True
-            m = self.base[s] + n
-            if self.check[m] != s:  # check为0
-                return False
-        pass
+    def compare_tails(self, start, word, i):
+        pos = -self.base[start]
+        tail1 = self.tails[pos]
+        tail2 = word[i+1:] + '#'  # 通过'#'字符来规避一个字符串是另一个字符串的子串的情况
+        compare_result = True if tail1 == tail2 else False
+        return compare_result, pos, tail1, tail2
+
+    def search(self, word):
+        if not self.check_word(word):
+            return False
+        start = self.root
+        for i, c in enumerate(word):
+            arc = self.chars[c]
+            end = self.base[start] + arc
+            if self.check[end] == 0:
+                return False, None
+            elif self.check[end] != start:
+                return False, self.get_node_strs(word[:i+1], start)
+            else:
+                start = end
+                if self.base[start] < 0:
+                    compare_result, pos, tail1, _ = self.compare_tails(start, word, i)
+                    if compare_result:
+                        return True, [word,]
+                    return False, [word[:i+1] + tail1[:-1],]
+
+    def get_node_strs(self, prefix, start):
+        result = []
+        for tail in self.get_node_tails(start):
+            result.append(prefix+tail)
+        return result
+
+    def get_node_tails(self, start):
+        if self.base[start] < 0:
+            return [self.tails[-self.base[start]][:-1],]
+        else:
+            arcs = self.find_arcs(start)
+            result = []
+            for arc in arcs:
+                end = self.base[start] + arc
+                tails = self.get_node_tails(end)
+                for tail in tails:
+                    result.append(arc+tail)
+            return result
 
     def delete(self, word):
-        pass
+        if not self.check_word(word):
+            return False
+        start = self.root
+        for i, c in enumerate(word):
+            arc = self.chars[c]
+            end = self.base[start] + arc
+            if self.check[end] == 0 or self.check[end] != start:
+                return False
+            else:
+                start = end
+                if self.base[start] < 0:
+                    compare_result, pos, _, _ = self.compare_tails(start, word, i)
+                    if compare_result:
+                        self.tails.pop(pos)
+                        self.base[start] = self.check[start] = 0
+                        return True
+                    return False
 
     def add(self, word):
-        # word += '#'
+        if not self.check_word(word):
+            return False
         self.chars.add_word(word)
         start = self.root
         for i, c in enumerate(word):
-            n = self.chars[c]
-            end = self.base[start] + n
-            if self.check[end] == 0:  # base[end]将会是独立节点
-                self.base[end] = -self.next_pos
-                self.check[end] = start
-                self.tails[self.next_pos] = word[i+1:] + '#'
-                self.next_pos += len(self.tails[self.next_pos])
-                break
-            else:  # 从check[end]到end的有向边已存在
-                start = self.check[end]
-                if self.base[start] < 0:  # base[start]是独立节点
-                    pos = -self.base[start]
-                    tail1 = self.tails[pos][:-1]
-                    tail2 = word[i+1:]
-                    if tail1 == tail2:  # 该word已存在
+            arc = self.chars[c]
+            end = self.base[start] + arc
+            if self.check[end] == 0:
+                # base[end]将会变成独立节点，后续字符串将会保存到tails中
+                self.write_tail(start, end, word[i+1:]+'#')
+                return True
+            elif self.check[end] == start:
+                # 从start到end的有向边已存在
+                start = end
+                if self.base[start] < 0:
+                    # start是独立节点，需要比较word的后续字符串和tails中的字符串
+                    compare_result, pos, tail1, tail2 = self.compare_tails(start, word, i)
+                    if compare_result:
+                        # 该word已存在
                         return True
-                    lcp = self.longest_common_prefix(tail1, tail2)
-                    q = self.x_check(lcp)
-                    self.base[start] = q
-                    for new_c in lcp:  # 存储公共前缀字符串
-                        new_n = self.chars[new_c]
-                        new_m = self.base[start] + new_n
-                        self.check[new_m] = start
-                        start = new_m
+
+                    lcp = self.longest_common_prefix(tail1, tail2)  # 找到最长公共子串
+                    if lcp is not '':
+                        q = self.x_check(lcp)
+                        for new_c in lcp:  # 存储公共前缀字符串
+                            self.base[start] = q
+                            # 下一个节点
+                            start = self.add_arc(new_c, start)
+
                     len_lcp = len(lcp)
-                    x = tail1[len_lcp:]
-                    y = tail2[len_lcp:]
+                    # x != y 一定成立
+                    x, y = tail1[len_lcp:], tail2[len_lcp:]
                     q = self.x_check(x[0]+y[0])
 
                     self.base[start] = q
-                    n = self.chars[x[0]]
-                    end = self.base[start] + n
+                    # 将x保存到tails中
+                    end = self.add_arc(x[0], start)
                     self.base[end] = -pos
-                    self.check[end] = start
-                    self.tails[pos] = x + '#'
+                    self.tails[pos] = x[1:]
+                    # 将y保存到tails中
+                    end = self.add_arc(y[0], start)
+                    self.write_tail(start, end, y[1:])
+                    return True
+            else:
+                # 从start到end的有向边不存在，此时发生冲突
+                old_start = self.check[end]
+                conflict = {
+                    start: self.find_arcs(start),
+                    old_start: self.find_arcs(old_start),
+                }
+                if len(conflict[start]) + 1 > len(conflict[old_start]):
+                    change_node = old_start
+                else:
+                    change_node = start
+                temp_base = self.base[change_node]
+                q = self.x_check(conflict[change_node])
+                self.base[change_node] = q
+                for c in conflict[change_node]:
+                    arc = self.chars[c]
+                    old_end = temp_base + arc
+                    new_end = self.base[change_node] + arc
+                    self.base[new_end] = self.base[old_end]
+                    self.check[new_end] = self.check[old_end]
+                    if self.base[old_end] > 0:
+                        for key, value in self.check.iteritems():
+                            if value == old_end:
+                                self.check[key] = new_end
+                    self.base[old_end] = self.check[old_end] = 0
 
-                    n = self.chars[y[0]]
-                    end = self.base[start] + n
-                    self.base[end] = -self.next_pos
-                    self.check[end] = start
-                    self.tails[self.next_pos] = y + '#'
-                    self.next_pos += len(self.tails[self.next_pos])
-
-        pass
+                self.write_tail(start, end, word[i+1:]+'#')
+                return True
 
     def longest_common_prefix(self, *words):
         lcp = ''
-        for _, item in enumerate(zip(*words)):
+        for item in zip(*words):
             if len(set(item)) == 1:
                 lcp += item[0]
             else:
@@ -133,6 +204,31 @@ class DoubleArrayTrie(object):
                 break
             q += 1
         return q
+
+    def write_tail(self, start, end, word):
+        self.base[end] = -self.next_pos
+        self.check[end] = start
+        self.tails[self.next_pos] = word
+        self.next_pos += len(self.tails[self.next_pos])
+
+    def add_arc(self, c, start):
+        arc = self.chars[c]
+        end = self.base[start] + arc
+        self.check[end] = start
+        return end
+
+    def find_arcs(self, start):
+        arcs = []
+        reverse_chars = {v:k for k,v in self.chars}
+        for key, value in self.check.iteritems():
+            if value == start:
+                arc = key - self.base[start]
+                arcs.append(reverse_chars[arc])
+        return arcs
+#        for c in self.chars:
+#            if self.check[self.base[start] + self.chars[c]] == start:
+#                arcs.append(c)
+#        return arcs
 
     def search_exists(self, word):
         pass
